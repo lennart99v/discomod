dbbeda2549808d61b9ddfbd1f5bf9016bacf9446
Lennart Verschelden <lennartverschelden1999@hotmail.com>
2025-05-25 00:11:38 +0200

    basic setup

--- src/main/java/net/lennart/discomod/DiscoMod.java
+++ src/main/java/net/lennart/discomod/DiscoMod.java
@@ -1,6 +1,10 @@
 package net.lennart.discomod;
 
 import com.mojang.logging.LogUtils;
+import net.lennart.discomod.block.ModBlocks;
+import net.lennart.discomod.item.ModCreativeModeTabs;
+import net.lennart.discomod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -21,7 +25,9 @@ public class DiscoMod {
 
     public DiscoMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
+        ModCreativeModeTabs.register(modEventBus);
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -34,7 +40,10 @@ public class DiscoMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
